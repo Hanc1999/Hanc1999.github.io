060c0b9b8277af474369b1d0e6c98bb7e708d3d7
Hanc1999 <39997275+Hanc1999@users.noreply.github.com>
2025-04-17 02:53:04 +0000

    Deploying to gh-pages from @ Hanc1999/Hanc1999.github.io@76c665e5a93db63376fed279cad0e2b1dc820491 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,14 +440,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%79%69%6D%69%6E%73%68%69@%75.%6E%75%73.%65%64%75", "_blank");
         },
       },{
         id: 'social-rss',
@@ -461,7 +454,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=qVUAdW0AAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
